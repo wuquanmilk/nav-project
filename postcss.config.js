a4ed4e31be317eade44e346ba91f28baac04528a
杭州全叔 <128679244+wuquanmilk@users.noreply.github.com>
2025-11-30 19:58:09 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-// postcss.config.js
 export default {
   plugins: {
-    // 核心修复：根据构建日志中的错误，使用这个包名
-    '@tailwindcss/postcss': {},
-    // 保持 Autoprefixer 不变
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
